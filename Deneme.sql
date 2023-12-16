ea050623e4c259232e949864a29447ef49f8aee5
mbaransen <94108242+mbaransen@users.noreply.github.com>
2023-12-16 14:03:56 +0300

    Add files via upload

---
+++ Deneme.sql
@@ -0,0 +1,4 @@
+USE barandb;
+SELECT * FROM Musteri;
+-- belirli bir kritere göre sralama yapmak
+ORDER BY ad ASC;--  ASC komutu isme göre sralama yapti A dan Z ye dogru. DESC komutu Z den A ya dogru yazdirir.
